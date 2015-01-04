836ab674235b3d4b1d8b8038777bb2ab2772e47a
vitaminwater <stantin04@gmail.com>
2015-01-04 18:19:23 +0100

    first commit

---
+++ bionicohand3.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <boards.h>
+#include <RBL_nRF8001.h>
+
+
+// the setup routine runs once when you press reset:
+void setup() {    
+  // Init. and start BLE library.
+  ble_begin();
+ 
+  // Enable serial debug
+  Serial1.begin(115200);
+ 
+}
+
+char data_in;
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  if ( ble_available() )
+  {
+    while ( ble_available() ) {
+      Serial1.write(ble_read());
+    }
+  }
+  
+  if ( Serial1.available() )
+  {
+    
+    while ( Serial1.available() )
+    ble_write( Serial1.read() );
+  }
+  
+  ble_do_events();
+ 
+}
